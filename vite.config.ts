1c1031b3de29a8572181973ae5cba0f01a276c59
kndyman2-design <kndyman2@gmail.com>
2026-02-03 20:47:24 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.VITE.GEMINI_API_KEY': JSON.stringify(env.VITE.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
